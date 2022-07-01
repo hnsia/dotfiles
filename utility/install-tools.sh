7ba4ab29afe10e328d56e359ee7a25b2721e5411
hnsia <44498178+hnsia@users.noreply.github.com>
2022-07-01 08:34:02 +0800

    chore: add installation steps for zsh and nvm

--- utility/install-tools.sh
+++ utility/install-tools.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 # Meta-packages
+echo '[*] Installing meta-packages...'
 sudo apt update
 sudo apt install build-essential -y
 
 # Git
+echo '[*] Installing Git...'
 sudo apt-add-repository ppa:git-core/ppa
 sudo apt update
 sudo apt install git -y
+
+# Zsh(shell) and Oh My Zsh(Zsh config managing framework)
+echo '[*] Installing Zsh...'
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Stow sym links of configurations to home directory
 echo '[*] Removing default configurations...'
@@ -15,3 +22,8 @@ rm ~/.gitconfig ~/.zshrc
 
 echo '[*] Stowing/Creating simlinks for git, zsh, aliases...'
 stow -vSt ~ git zsh
+
+# NVM and Node
+echo '[*] Installing nvm and node...'
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install --lts
