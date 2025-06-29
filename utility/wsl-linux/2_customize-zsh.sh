f418b47ec657588389c9843f7e72e9f0fcdcb2e2
hnsia <44498178+hnsia@users.noreply.github.com>
2025-06-29 15:17:31 +0800

    chore: update installation for neovim

--- utility/wsl-linux/2_customize-zsh.sh
+++ utility/wsl-linux/2_customize-zsh.sh
@@ -6,8 +6,3 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # Install Powerlevel10k zsh theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# NVM
-echo '[*] Installing nvm and node...'
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-zsh
