35d437c7ddd379e972d8006ebb165b1f46e072e7
hnsia <44498178+hnsia@users.noreply.github.com>
2025-07-10 22:54:35 +0800

    feat: add lsp autocompletion to typecraft template

--- nvim-templates/typecraft/lua/plugins/lsp.lua
+++ nvim-templates/typecraft/lua/plugins/lsp.lua
@@ -1,14 +1,23 @@
 return {
-  "mason-org/mason-lspconfig.nvim",
-  opts = {
-    ensure_installed = { "lua_ls", "ts_ls" },
-  },
-  dependencies = {
-    { "mason-org/mason.nvim", opts = {} },
-    { "neovim/nvim-lspconfig",
-      keys={
-        { "<leader>ca", vim.lsp.buf.code_action, desc = "Execute a code action" },
-      }
-    },
-  },
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = { "lua_ls", "ts_ls" },
+	},
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		{
+			"neovim/nvim-lspconfig",
+			keys = {
+				{ "<leader>ca", vim.lsp.buf.code_action, desc = "Execute a code action" },
+			},
+			config = function()
+				local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+				local lspconfig = require("lspconfig")
+				lspconfig.lua_ls.setup({
+					capabilities = capabilities,
+				})
+			end,
+		},
+	},
 }
