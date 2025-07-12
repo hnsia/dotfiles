ed03d17f88202379136020e9f9125fcfd114f122
hnsia <44498178+hnsia@users.noreply.github.com>
2025-07-12 12:09:46 +0800

    feat: add support for go lsp debugger in typecraft template

--- nvim-templates/typecraft/lua/plugins/debugging.lua
+++ nvim-templates/typecraft/lua/plugins/debugging.lua
@@ -1,26 +1,30 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio"
-  },
-  config = function()
-    local dap, dapui = require("dap"), require("dapui")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+		"leoluz/nvim-dap-go",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
 
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+      require("dapui").setup()
+		require("dap-go").setup()
 
-    vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, {})
-    vim.keymap.set("n", "<F5>", dap.continue, {})
-  end,
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>b", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<F5>", dap.continue, {})
+	end,
 }
